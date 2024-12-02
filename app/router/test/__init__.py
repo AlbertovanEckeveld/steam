cf67573d7040b3d7b81f8be9166f6d5c9f86c041
Alberto van Eckeveld <67636045+AlbertovanEckeveld@users.noreply.github.com>
2024-12-02 22:54:19 +0100

    verwijderen config.py en connector folder aangemaakt

--- app/router/test/__init__.py
+++ app/router/test/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from app.db import get_version, load_environment_variables
+from app.connector.database import get_version, load_environment_variables
 
 Test = Blueprint('test', __name__, static_folder='app/static', template_folder='app/templates')
 
@@ -9,6 +9,5 @@ def index():
 
     DB_version = get_version()
     env_data = load_environment_variables()
-
 
     return render_template("test.html", version=DB_version, env_data=env_data)
