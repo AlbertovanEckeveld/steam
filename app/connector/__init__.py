9ad7956f54f3b7a8713f85380eeb8c6510bc8ed5
Alberto van Eckeveld <67636045+AlbertovanEckeveld@users.noreply.github.com>
2024-12-12 01:24:53 +0100

    env file uitgeforceerd

--- app/connector/__init__.py
+++ app/connector/__init__.py
@@ -1,38 +1,32 @@
-
-import os
-from dotenv import load_dotenv
+from app.config import DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD, SECRET_KEY, STEAM_API_KEY
 
 def get_database_info():
     """
     Load database info variables from .env file.
     """
-    load_dotenv()
     return {
-        'DB_HOST': os.getenv('DB_HOST'),
-        'DB_PORT': os.getenv('DB_PORT'),
-        'DB_DATABASE': os.getenv('DB_DATABASE')
+        'DB_HOST': DB_HOST,
+        'DB_PORT': DB_PORT,
+        'DB_DATABASE': DB_DATABASE
     }
 
 def get_user_info():
     """
     Load user info variables from .env file.
     """
-    load_dotenv()
     return {
-        'DB_USER': os.getenv('DB_USER'),
-        'DB_PASSWORD': os.getenv('DB_PASSWORD')
+        'DB_USER': DB_USER,
+        'DB_PASSWORD': DB_PASSWORD
     }
 
 def get_steam_API():
     """
     Load steam api variables from .env file.
     """
-    load_dotenv()
-    return os.getenv('STEAM_API')
+    return STEAM_API_KEY
 
 def get_secret_key():
     """
     Load secret key variables from .env file.
     """
-    load_dotenv()
-    return os.getenv('SECRET_KEY')
+    return SECRET_KEY
