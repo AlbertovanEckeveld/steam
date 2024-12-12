154614cef64c111e266f316c0b148e3ff1ba88f4
Alberto van Eckeveld <67636045+AlbertovanEckeveld@users.noreply.github.com>
2024-12-12 18:57:29 +0100

    get user profile data fix

--- app/router/test/__init__.py
+++ app/router/test/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, session
 
 from app.connector import get_database_info, get_user_info
 from app.connector.database import get_version
+from app.models import UserProfile
 
 Test = Blueprint('test', __name__, static_folder='app/static', template_folder='app/templates')
 
@@ -14,7 +15,10 @@ def index():
     DB_database = database_info.get('DB_DATABASE')
     DB_user = get_user_info()['DB_USER']
     if session.get('user'):
-        Steam_user = [session.get('user')['display_name'], session.get('user')['steam_id'], session.get('user')['url_avatar_small']]
+
+        user_profile_data = session.get('user_profile')
+        user = UserProfile(**user_profile_data) if user_profile_data else None
+
         logged_in = True
         return render_template("test.html",
                                version=DB_version,
@@ -23,9 +27,9 @@ def index():
                                DB_database=DB_database,
                                DB_user=DB_user,
                                logged_in=logged_in,
-                               display_name=Steam_user[0],
-                               steam_id=Steam_user[1],
-                               steam_avater=Steam_user[2]
+                               display_name=user.get_displayname(),
+                               steam_id=user.get_steam_id(),
+                               steam_avater=user.get_avatar_small()
                                )
     else:
         logged_in = False
