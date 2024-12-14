918b9bb2cf956231bded31cc7f15bdff16fc2b38
Webserv-User <webserv@steam-project.com>
2024-12-14 21:32:32 +0000

    Update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -40,30 +40,34 @@ if [  -d "${GIT_DIR}" ]; then
     echo -e "${BOLD_GREEN}Repository gevonden${NC}"
 
     # Controleer of de huidige branch de productie-branch is
-    if [ "$(git branch --show-current)" != "prod_webserv" ]; then
+    if [ "$(sudo -u ${REQUIRED_USER} git branch --show-current)" != "prod_webserv" ]; then
         echo -e "${BOLD_YELLOW}Huidige branch is niet de productie-branch.. ${YELLOW}Overschakelen naar: "origin/prod_webserv"${NC}"
-        git checkout origin/prod_webserv
+        sudo -u ${REQUIRED_USER} git checkout origin/prod_webserv
+        sudo -u ${REQUIRED_USER} git add . > /dev/null 2>&1
+        sudo -u ${REQUIRED_USER} git commit -m "Update-script: $(date +'%Y-%m-%d %H:%M:%S') - Files: $(sudo -u ${REQUIRED_USER} git status --porcelain | awk '{print $2}')"
+        sudo -u ${REQUIRED_USER} git push origin $(sudo -u ${REQUIRED_USER} git branch --show-current)
+        sudo -u ${REQUIRED_USER} git checkout origin/prod_webserv
     fi
 
     # Controleer of er niet-gecommiteerde wijzigingen zijn
-    if [ "$(git status --porcelain)" ]; then
+    if [ "$(sudo -u ${REQUIRED_USER} git status --porcelain)" ]; then
         echo -e "${BOLD_YELLOW}Er zijn niet-gecommiteerde wijzigingen.. ${YELLOW}Commit wijzigingen${NC}"
-        git add . > /dev/null 2>&1
-        files=$(git status --porcelain | awk '{print $2}')
-        git commit -m "Update-script: $(date +'%Y-%m-%d %H:%M:%S') - Files: $files"
-        git push origin prod_webserv > /dev/null 2>&1
+        sudo -u ${REQUIRED_USER} git add . > /dev/null 2>&1
+        files=$(sudo -u ${REQUIRED_USER} git status --porcelain | awk '{print $2}')
+        sudo -u ${REQUIRED_USER} git commit -m "Update-script: $(date +'%Y-%m-%d %H:%M:%S') - Files: $files"
+        sudo -u ${REQUIRED_USER} git push origin prod_webserv 
         echo -e "${BOLD_GREEN}Wijzigingen succesvol gecommit & gepushed: ${files}"
     fi
 
     # Controleer of de repository up-to-date is
-    git fetch origin prod_webserv > /dev/null 2>&1
+    sudo -u ${REQUIRED_USER} git pull origin/prod_webserv > /dev/null 2>&1
 
-    if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+    if [ "$(sudo -u ${REQUIRED_USER} git rev-parse HEAD)" != "$(sudo -u ${REQUIRED_USER} git rev-parse @{u})" ]; then
         echo -e "${BOLD_YELLOW}Repository is verouderd, ${YELLOW}nu bijwerken..${NC}"
         
         # Controleer of merge is mogelijk
-        if git merge-base @{u} HEAD; then
-            git merge origin/prod_webserv
+        if sudo -u ${REQUIRED_USER} git merge-base @{u} HEAD; then
+            sudo -u ${REQUIRED_USER} git merge origin/prod_webserv
         else
             echo -e "${RED}Merge is niet mogelijk${NC}"
             exit 1
