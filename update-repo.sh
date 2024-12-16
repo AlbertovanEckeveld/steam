935cc6a1c1601619c48359f83b0165f57ad74d66
Webserv-User <webserv@steam-project.com>
2024-12-16 01:28:27 +0000

    Update-script: 2024-12-16 01:28:27 - Files: setup.sh update-repo.sh update.sh

--- update-repo.sh
+++ update-repo.sh
@@ -52,7 +52,7 @@ if [  -d "${GIT_DIR}" ]; then
     # Controleer of de huidige branch de productie-branch is
     if [ "$(sudo -u ${REQUIRED_USER} git branch --show-current)" != "prod_webserv" ]; then
         echo -e "${BOLD_YELLOW}Huidige branch is niet de productie-branch.. ${YELLOW}Overschakelen naar: "origin/prod_webserv"${NC}"
-        sudo -u ${REQUIRED_USER} git switch origin/prod_webserv
+        sudo -u ${REQUIRED_USER} git switch prod_webserv
     fi
 
     # Controleer of de repository up-to-date is
